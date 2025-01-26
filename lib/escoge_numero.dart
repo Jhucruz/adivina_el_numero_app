a712b2ea39e247b1666f4a3ed73cf750932370b7
Paulo Rodriguez <paulo_crr@hotmail.com>
2025-01-25 19:40:10 -0500

    chore: Correcciones

--- lib/escoge_numero.dart
+++ lib/escoge_numero.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class EscogeNumero extends StatefulWidget {
-  const EscogeNumero({super.key});
+  final int numeroAdivinar;
+  const EscogeNumero({super.key, required this.numeroAdivinar});
 
   @override
   State<EscogeNumero> createState() => _EscogeNumero();
@@ -9,6 +10,8 @@ class EscogeNumero extends StatefulWidget {
 
 class _EscogeNumero extends State<EscogeNumero> {
   var numeroActual = 60.0;
+  var adivino = false;
+  int? numeroSeleccionado;
 
   @override
   Widget build(BuildContext context) {
@@ -18,23 +21,53 @@ class _EscogeNumero extends State<EscogeNumero> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text("¡Estoy pensando en un mumero adivina cual es¡"),
-          Text("Creo que es"),
-          Text("${numeroActual.toInt()}"),
-          Slider(
-              min: 1,
-              max: 120,
-              value: numeroActual,
-              onChanged: (nuevoValor) {
+          if (adivino) ...[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("¡Adivinaste!"),
+              ],
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("Volver"),
+            )
+          ] else ...[
+            Text("¡Estoy pensando en un mumero adivina cual es¡"),
+            Text("Creo que es"),
+            if (numeroSeleccionado != null &&
+                numeroSeleccionado! > widget.numeroAdivinar) ...[
+              Text("Muy alto"),
+            ] else if (numeroSeleccionado != null &&
+                numeroSeleccionado! < widget.numeroAdivinar) ...[
+              Text("Muy bajo"),
+            ],
+            Text("${numeroActual.toInt()}"),
+            Slider(
+                min: 1,
+                max: 120,
+                value: numeroActual,
+                onChanged: (nuevoValor) {
+                  setState(() {
+                    numeroActual = nuevoValor;
+                  });
+                }),
+            Text("utiliza el Slider para adivina"),
+            ElevatedButton(
+              onPressed: () {
+                //TODO: Implementar la logica de adivinar el numero
                 setState(() {
-                  numeroActual = nuevoValor;
+                  numeroSeleccionado = numeroActual.toInt();
+                  if (numeroSeleccionado == widget.numeroAdivinar) {
+                    adivino = true;
+                  }
                 });
-              }),
-          Text("utiliza el Slider para adivina"),
-          ElevatedButton(
-            onPressed: () {},
-            child: Text("EMPEZAR"),
-          )
+              },
+              child: Text("EMPEZAR"),
+            )
+          ]
         ],
       ),
     );
